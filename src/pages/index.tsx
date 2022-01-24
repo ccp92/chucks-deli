05d4b5a68625b6db0eb462962b227f19b827c005
Chris Parsons <mail@chrisparsons.dev>
2022-01-24 21:44:12 +0000

    Add some css to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,52 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Menu" />
-    <StaticImage src="../images/chucks.JPG" alt="Chuck's Deli & Convenience" style={{
-      maxHeight: "20%"
-    }} />
-    <h1>Menu</h1>
-    <p>
-      <Link to="/breakfast">Breakfast</Link> <br />
-      <Link to="/lunch/">Lunch</Link> <br />
-      <Link to="/daily-specials/">Daily Specials</Link> <br />
-      <Link to="/catering/">Catering</Link> <br />
-    </p>
+    <StaticImage
+      src="../images/chucks.JPG"
+      alt="Chuck's Deli & Convenience"
+      style={{
+        maxHeight: "20%",
+      }}
+    />
+    <h1 style={{ paddingTop: "0.5em", textAlign: "center" }}>Menu</h1>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div
+        style={{
+          width: "50%",
+          textAlign: "center",
+          padding: "2em 0",
+        }}
+      >
+        <Link to="/breakfast">Breakfast</Link> <br />
+      </div>
+      <div
+        style={{
+          width: "50%",
+          textAlign: "center",
+          padding: "2em 0",
+        }}
+      >
+        <Link to="/lunch/">Lunch</Link> <br />
+      </div>
+      <div
+        style={{
+          width: "50%",
+          textAlign: "center",
+          padding: "2em 0",
+        }}
+      >
+        <Link to="/daily-specials/">Daily Specials</Link> <br />
+      </div>
+      <div
+        style={{
+          width: "50%",
+          textAlign: "center",
+          padding: "2em 0",
+        }}
+      >
+        <Link to="/catering/">Catering</Link> <br />
+      </div>
+    </div>
   </Layout>
 )
 
