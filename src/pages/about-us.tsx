61127214d0f90e282e5ce9023ab1c3b0acccc87f
Chris Parsons <mail@chrisparsons.dev>
2022-01-24 15:15:53 +0000

    Add about us section

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -7,7 +7,31 @@ const BreakfastPage = () => (
   <Layout>
     <Seo title="About Us" />
     <h1>About Us</h1>
-    <p>Chuck's Deli & Convenience</p>
+    <p>Welcome to Chuck's Deli & Convenience</p>
+    <p>
+      Chuck's Deli & Convenience and Brown's Cleaners made their début in the
+      Train Yards in 2007. It was a momentous occasion — one that required that
+      we faithfully put our savings into what seemed like a fruitless
+      run-of-the-mill, over-the-counter deli along with a dry cleaning service.
+      With little more than a few good menu items, we began our journey into a
+      brand-new world. But lo and behold, in a year of introducing the famous
+      Chuck's Panini, our panini sandwich spoke for itself.
+    </p>
+
+    <p>
+      What makes our food stand out? For starters, there are no fryers on the
+      premises. All of our food is baked, which means that you’ll get only the
+      highest-quality foods. Daily fresh bread is delivered early in the morning
+      to accommodate the famous freshly baked chicken, filet mignon steak and
+      numerous other delights, including delicious samosas and spinach pies —
+      all prepared by the loving hands of our ladies in the kitchen.
+    </p>
+
+    <p>
+      If you haven’t tried our food, now’s the time. Maybe you, too, will join
+      the ranks of the faithful. And, you can drop off your cleaning at the same
+      time.
+    </p>
   </Layout>
 )
 
