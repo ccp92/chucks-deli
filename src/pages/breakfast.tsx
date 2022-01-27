3d7fbcc3e260ac5b9f5b84af01f562571ce0faeb
Chris Parsons <mail@chrisparsons.dev>
2022-01-27 21:22:35 +0000

    Add Breakfast menu

--- src/pages/breakfast.tsx
+++ src/pages/breakfast.tsx
@@ -1,13 +1,30 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import MenuSection from "../components/menu"
 import Seo from "../components/seo"
 
 const BreakfastPage = () => (
   <Layout>
-    <Seo title="Breakfast" />
-    <h1>Breakfast</h1>
-    <p>Yummy Breakfast Food!</p>
+    <Seo title="Breakfast Menu" />
+    <h1>Breakfast Menu</h1>
+    <p>Sandwiches served on toast, bagel or english muffin</p>
+    <MenuSection header="" anchor={""}>
+      <li>One Egg Sandwich: $2.49</li>
+      <li>Two Egg Western Sandwich: $4.49</li>
+      <li>B.L.T: $3.99</li>
+      <li>B.E.L.T: $4.99</li>
+      <li>Swiss Breakfast: $4.39</li>
+      <li>Croissant Sandwich with egg, ham & cheese: $4.39</li>
+      <li>Grilled Cheese Sandwich: $3.99 (add bacon +$1.00)</li>
+      <li>Bagel with Cream Cheese: $2.99</li>
+      <li>Breakfast Platter: $7.99</li>
+      <p>2 eggs with bacon, or ham, or sausage, home fries & coffee</p>
+      <li>Home Fries: $2.49</li>
+      <li>Panini Breakfast: $4.99</li>
+      <li>Three Egg Western Omelette with Home Fries: $7.99</li>
+      <p>Western, Vegetarian, or Three Cheese</p>
+    </MenuSection>
   </Layout>
 )
 
