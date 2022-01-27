9e7511e2c68a8773177a4c5f3dd41717016feae7
Chris Parsons <mail@chrisparsons.dev>
2022-01-27 21:26:46 +0000

    Update Daily Special menu

--- src/pages/daily-specials.tsx
+++ src/pages/daily-specials.tsx
@@ -1,13 +1,18 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import MenuSection from "../components/menu"
 import Seo from "../components/seo"
 
 const DailySpecialsPage = () => (
   <Layout>
     <Seo title="Daily Specials" />
-    <h1>Daily Specials</h1>
-    <p>Panini Sandwich with soup or salad & canned soft drink: $10.62 + tax</p>
+    <h1>Daily Special</h1>
+    <MenuSection header="Panini Sandwich" anchor="panini">
+      <p>
+        Panini Sandwich with soup or salad & canned soft drink: $10.62 + tax
+      </p>
+    </MenuSection>
   </Layout>
 )
 
